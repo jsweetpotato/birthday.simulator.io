9d3891fbd6d9e9c0cd9055bc23bbad1bd8dbb3b8
김지수 <jsweetpotato37@gmail.com>
2022-01-09 02:07:14 +0900

    add cream

--- docs/app.js
+++ docs/app.js
@@ -1,21 +1,52 @@
 const side = document.querySelector('.side');
-let sideElem = [];
+const creamGroup = document.querySelector('.creams');
+const cream = document.querySelector('.cream');
+const creamTop = document.querySelector('.cream-top');
 
-function init() {
-  for (let i = 0; i <= 60; i++){
+let sideArr = [];
+let creamTopArr = [];
+let creamGroupArr = [];
+
+// size
+let creamD = 160
+
+function makingCake(num, array, name, degNum, width) {
+  for (let i = 0; i < num; i++){
     let list = document.createElement('li');
-    list.classList.add(`side${i}`);
-    sideElem.push(list);
-    side.appendChild(list);
+    array.push(list);
+    name.appendChild(list);
   }
-
-  for (let i = 0; i <= 60; i++){
-    let deg = i * 6;
-    let transZ = 21/Math.tan(6 * Math.PI / 180);
-    sideElem[i].style.transform = `rotateY(${deg}deg) translateZ(${transZ}px)`;
+  
+  for (let i = 0; i < num; i++){
+    let deg = i * degNum;
+    let tanZ = width / Math.tan(degNum * Math.PI / 180);
+    array[i].style.transform = `rotateY(${deg}deg) translateZ(${tanZ}px)`
   }
 }
 
 
+function makingCream() {
+  makingCake(6, creamTopArr, creamTop, 60, 30);
 
-init();
+  let count = 5;
+  creamGroupArr.push(cream);
+  
+  for (let i = 0; i < count; i++){
+    let createCream = cream.cloneNode(true);
+    creamGroup.appendChild(createCream);
+    creamGroupArr.push(createCream);
+    console.log(creamGroupArr);
+  }
+  
+  for (let i = 0; i <= count; i++){
+    let deg = i * 60;
+    creamGroupArr[i].style.transform = `rotateY(${deg}deg) translateZ(${creamD}px)`
+  }
+}
+
+function init() {
+  makingCake(60, sideArr, side, 6, 21);
+  makingCream();
+}
+
+init()
